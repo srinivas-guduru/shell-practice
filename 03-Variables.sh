1c14180aefd1e6ae4607105fc0ff5b033fab258d
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-28 20:39:20 +0530

    updat

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+
+echo "$PERSON1: : Hello $PERSON2 ,How are you?"
+echo "$PERSON2: : Hi $PERSON1, am fine thanks for asking ,How are you?"
+echo "$PERSON1: : doing fine, what are you learning $PERSON2?"
+echo "$PERSON2: : 1 am learning Shell, What about you?"
