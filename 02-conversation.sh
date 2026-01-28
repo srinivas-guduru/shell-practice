b45c8ced29b7c46dfa8e45e08037150977813d72
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-28 19:41:58 +0530

    updat

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Suresh: : Hello Ramesh ,How are you?"
+echo "Ramesh: : Hi suresh, am fine thanks for asking ,How are you?"
+echo "Suresh: : doing fine, what are you learning Ramesh?"
+echo "Ramesh: : 1 am learning Shell, What about you?"
