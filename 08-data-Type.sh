c7630d59f9c9e02465e3f2590997ea0d751de41e
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-29 17:16:57 +0530

    change

--- 08-data-Type.sh
+++ 08-data-Type.sh
@@ -11,5 +11,6 @@ echo "Sum is :$SUM"
 FRUITS=("Apple" "Banana" "Pomo")
 
 echo "Fruits are: ${FRUITS[@]}"
-
-
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
