ccec28f0b51e0edbfb7a77dce2309f997df9b56d
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-29 18:08:57 +0530

    change

--- 09-Conditions.sh
+++ 09-Conditions.sh
@@ -2,11 +2,11 @@
 
 NUMER=25
 
--gt -> freater than
--it -> less than
--eq -> euqal
--na -> not equal
+# -gt -> freater than
+# -it -> less than
+# -eq -> euqal
+# -na -> not equal
 if [$NUMER -gt 20 ]; then
-echo "Given number: $NUMBER is greater than 20"
+ echo "Given number: $NUMBER is greater than 20"
 
 fi 
