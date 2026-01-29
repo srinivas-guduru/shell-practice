761ad3fa705cdc1a5cb97034c3a01e7859f7fbd2
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-29 21:39:25 +0530

    change

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,8 +6,10 @@ if [ $USERID -ne 0 ]; then
    echo "Please run this script with root user access"
    exit 1
 fi
+
     echo "installing Nginx"
     dnf install nginx -y
+    
 if [ $? -ne 0 ]; then
     echo "installing Nginx ... FAILURE"
     exit1
