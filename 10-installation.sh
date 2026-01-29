9c21ba0c110d5b02d5048e49c6089aac40b23adb
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-29 21:33:39 +0530

    change

---
+++ 10-installation.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+echo "Please run this script with root user access"
+exit 1
+fi
+echo "installing Nginx"
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+echo "installing Nginx ... FAILURE"
+exit1
+else
+echo "installing nginx ... SUCCESS"
+fi
