15ca23ddd19e008f8cf03f1387fa835312824362
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-29 14:09:02 +0530

    change

--- 06-Timestamp-vairbles.sh
+++ 06-Timestamp-vairbles.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-STAT_TIME=$(date +%s) 
+START_TIME=$(date +%s) 
 
 echo "Script executed at: $TIMESTAMP"
 
@@ -8,7 +8,6 @@ sleep 10
 
 END_TIME=$(date +%s) 
 TOTAL_TIME=$(($END_TIME-$START_TIME))
-
 
 echo "Script executed in: $TOTAL_TIME"
 
