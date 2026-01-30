0f1d5b760d7c8216154ae8af5283be99ea6d3279
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-30 19:50:24 +0530

    complete

---
+++ 11-functions.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+   echo "Please run this script with root user access"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2...FAILURE"
+      exit1
+    else
+       echo "$2 ...SUCCESS"
+     fi
+}       
+dnf install nginx -y
+VALIDATE $?  "installing Niginx"
+
+dnf install mysql -y
+VALIDATE $?  "installing mysql"
+
+dnf install nginx -y
+VALIDATE $?  "installing  nodejs"git 
