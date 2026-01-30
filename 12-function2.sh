991c18cfbafe881cc2f7e772ab708497794fd562
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-30 20:54:08 +0530

    complete

--- 12-function2.sh
+++ 12-function2.sh
@@ -3,14 +3,17 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-   echo "Please run this script with root user access"
-   exit 1
+   echo "$2 ... **FAILURE**" 
+else 
+    echo "$2 ... SUCCESS"
 fi
+
+echo "" 
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
       echo "$2 ... FAILURE"
-      exit 1
+    
     else
       echo "$2 ... SUCCESS"
  fi
