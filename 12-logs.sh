413ac46dc0cea7e1e8f18ac55e54878c91563db6
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-31 11:52:33 +0530

    complete

--- 12-logs.sh
+++ 12-logs.sh
@@ -11,18 +11,18 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-      echo "$2 ... FAILURE"
+      echo "$2 ... FAILURE" | tee -a $LOGS_FILE
       exit 1
     else
-      echo "$2 ... SUCCESS"
+      echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
  fi
 }   
 
-dnf install nginx -y &>> $LOGS_FILE
+dnf install nginx -y &>>  | tee -a $LOGS_FILE
 VALIDATE $?  "installing Niginx"
 
-dnf install  mysql -y &>> $LOGS_FILE
+dnf install  mysql -y &>> | tee -a $LOGS_FILE
 VALIDATE $?  "installing mysql"
 
-dnf install nodejs -y &>> $LOGS_FILE
+dnf install nodejs -y &>> | tee -a $LOGS_FILE
 VALIDATE $?  "installing  nodejs"
