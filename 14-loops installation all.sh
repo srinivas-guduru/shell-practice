b0a063a8638fb70b32006d58225e58f608e3defa
srinivasarao guduru <srinivas.guduru97@gmail.com>
2026-01-31 12:44:52 +0530

    completed

---
+++ 14-loops installation all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="var/log/shell-scripts"
+LOGS_FILE="/var/log/shell-scripts/$0.log"
+
+if [ $USERID -ne 0 ]; then
+   echo "Please run this script with root user access"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+      exit 1
+    else
+      echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+ fi
+}   
+
+for package in $@  # sudo sh 14-loops.sh nginx mysql nodejs
+do
+dnf install $package -y &>> $LOGS_FILE
+VALIDATE $?  "$package installation"
+done
